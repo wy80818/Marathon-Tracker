aa0b9591ffdd1a0a2d2aa591fc7781bec25ba4a8
wy80818 <jkotoulek@gmail.com>
2026-06-26 14:00:38 -0400

    speed insights this time

--- martoninfinity/src/main.tsx
+++ martoninfinity/src/main.tsx
@@ -1,10 +1,12 @@
 import { inject } from '@vercel/analytics';
+import { injectSpeedInsights } from '@vercel/speed-insights';
 import { BrowserRouter } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
 inject();
+injectSpeedInsights();
 
 createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
